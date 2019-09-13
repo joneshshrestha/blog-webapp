0a5a21da60c164f2231781b2acba8b3ed1603346
joneshshrestha <contact@joneshshrestha.com>
2019-09-13 18:44:59 +0545

    update

--- app.js
+++ app.js
@@ -12,9 +12,16 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.get('/', function (req, res) {
+  res.render("home", {
+    startingContent: homeStartingContent,
+    about: aboutContent,
+    contact: contactContent
+  })
+})
 
 
 
@@ -27,7 +34,6 @@ app.use(express.static("public"));
 
 
 
-
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
