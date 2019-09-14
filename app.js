f3355fdcaeae7edabe31a2851559fce81d0b45e4
joneshshrestha <contact@joneshshrestha.com>
2019-09-14 11:43:04 +0545

    update compose

--- app.js
+++ app.js
@@ -32,7 +32,11 @@ app.get('/compose', function (req, res) {
 })
 
 app.post('/compose', function (req, res) {
-  console.log(req.body.postTitle)
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  }
+  console.log(postObject)
 })
 
 
